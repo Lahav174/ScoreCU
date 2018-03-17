d8dda3dd45da86964dc2bc939c080499e33052a6
Lahav174 <lahav.lipson@columbia.edu>
2018-03-17 14:43:53 -0400

    Made checking string inclusion more robust

--- js/custom.js
+++ js/custom.js
@@ -632,6 +632,16 @@ function getCourseData(tree, courseID, question){
 	return output;
 }
 
+function robustIncludes(str, substr){
+	var substrings = substr.split(' ');
+	var output = true;
+	for (var i=0;i<substrings.length;i++){
+		if (!(str.includes(substrings[i])))
+			output = false;
+	}
+	return output;
+}
+
 function substrSearch(substr, question, showProfs){
 
 		var tree = firData
@@ -645,7 +655,7 @@ function substrSearch(substr, question, showProfs){
 		for (var courseID in tree["CourseIDs"]) {
 			if (tree["CourseIDs"].hasOwnProperty(courseID)) {
 
-				if (courseID.toLowerCase().includes(substr) || tree["CourseIDs"][courseID]["course_name"].toLowerCase().includes(substr)){
+				if (robustIncludes(courseID.toLowerCase(),substr) || robustIncludes(tree["CourseIDs"][courseID]["course_name"].toLowerCase(),substr)){
 
 
 					if (!showProfs){
@@ -678,7 +688,7 @@ function substrSearch(substr, question, showProfs){
 			for (var profName in tree["Profs"]) {
 				if (tree["Profs"].hasOwnProperty(profName)) {
 
-					if (profName.toLowerCase().includes(substr)){
+					if (robustIncludes(profName.toLowerCase(),substr)){
 
 						for (var courseID in tree["Profs"][profName]) {
 							if (tree["Profs"][profName].hasOwnProperty(courseID)) {
@@ -732,54 +742,3 @@ function substrSearch(substr, question, showProfs){
 
      	return text;
      }
-
-//Taken from http://stackoverflow.com/a/11958496/5057543
-function levDist(s, t) {
-    var d = []; //2d matrix
-
-    // Step 1
-    var n = s.length;
-    var m = t.length;
-
-    if (n == 0) return m;
-    if (m == 0) return n;
-
-    //Create an array of arrays in javascript (a descending loop is quicker)
-    for (var i = n; i >= 0; i--) d[i] = [];
-
-    // Step 2
-for (var i = n; i >= 0; i--) d[i][0] = i;
-	for (var j = m; j >= 0; j--) d[0][j] = j;
-
-    // Step 3
-for (var i = 1; i <= n; i++) {
-	var s_i = s.charAt(i - 1);
-
-        // Step 4
-        for (var j = 1; j <= m; j++) {
-
-            //Check the jagged ld total so far
-            if (i == j && d[i][j] > 4) return n;
-
-            var t_j = t.charAt(j - 1);
-            var cost = (s_i == t_j) ? 0 : 1; // Step 5
-
-            //Calculate the minimum
-            var mi = d[i - 1][j] + 1;
-            var b = d[i][j - 1] + 1;
-            var c = d[i - 1][j - 1] + cost;
-
-            if (b < mi) mi = b;
-            if (c < mi) mi = c;
-
-            d[i][j] = mi; // Step 6
-
-            //Damerau transposition
-            if (i > 1 && j > 1 && s_i == t.charAt(j - 2) && s.charAt(i - 2) == t_j) {
-            	d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
-            }
-        }
-    }
-    // Step 7
-    return d[n][m];
-}
